0395880c3972a9ff720de49521556e9ddbe4f522
pramitraych-os <pramitraych@gmail.com>
2026-07-27 01:32:34 +0530

    Time: 49 ms (45.51%) | Memory: 45.3 MB (78.23%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>m;
+        m[0]=1;
+        int s=0;int c=0;
+        for(int i:nums){
+            s+=i;
+            int t=s-k;
+            if(m.count(t)){
+                c+=m[t];
+            }
+            m[s]++;
+        }
+        return c;
+    }
+};
