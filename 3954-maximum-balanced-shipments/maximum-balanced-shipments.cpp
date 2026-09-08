6f0e7c0f433689016e605e59b766750e2d4dfb23
pramitraych-os <pramitraych@gmail.com>
2026-09-09 00:27:22 +0530

    Time: 4 ms (41.00%) | Memory: 153.8 MB (42.50%) - LeetSync

--- 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int maxBalancedShipments(vector<int>& weight) {
-        stack<int>s;
-        int c=0;
+        int c=0,mx=0;
         for(int i:weight){
-            bool f=false;
-            if(!s.empty()&&s.top()>i){
-                f=true;
-                s=stack<int>();
-            }
-            if(s.empty()&&f){
+            mx=max(i,mx);
+            if(i<mx){
                 c++;
-                continue;
+                mx=0;
             }
-            s.push(i);
         }
         return c;
     }
