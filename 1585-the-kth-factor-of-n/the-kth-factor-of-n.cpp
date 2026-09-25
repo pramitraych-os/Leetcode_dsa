e66f7cb9e9ef28f45dbd8034f076e87891c86b1f
pramitraych-os <pramitraych@gmail.com>
2026-09-25 23:28:36 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (48.64%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int ans=-1;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                k--;
+                if(k==0){
+                    ans=i;
+                }
+            }
+        }
+        return ans;
+    }
+};
